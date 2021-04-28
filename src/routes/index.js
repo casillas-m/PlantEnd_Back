4d17509dcbe191b87cdd1d8aaaeb61627a63254f
casillas-m <si715612@iteso.mx>
2021-04-27 23:53:34 -0500

    Password hashing added

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,31 +4,34 @@ const saltRounds = 10;
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-router.route("/").get((req,res)=>{
+router.route("/").get((req, res) => {
     res.json({
         alumnos: "MCR y MMM",
         servicio: "Proyecto Cloud"
     })
 })
 
-router.route("/register").post((req,res)=>{
+router.route("/register").post((req, res) => {
     let user = req.body
-    if(!(user.name&&user.email&&user.pass&&user.confirm)){
+    if (!(user.name && user.email && user.pass && user.confirm)) {
         //Falta algun dato
         res.sendStatus(418)
-    }else{
-        if(user.pass!=user.confirm){
+    } else {
+        if (user.pass != user.confirm) {
             //Password missmatch
             res.sendStatus(400)
-        }else{
+        } else {
             //Datos correctos, intentar registrar
-            //TODO: Integrar registro con base de datos
+            bcrypt.hash(user.pass, saltRounds).then(function (hash) {
+                //TODO: Integrar registro con base de datos
 
-            //Registro correcto
-            let token = jwt.sign({//Token de sesion (tambien lo retorna login)
-                email:user.email
-            }, process.env.JWT_SECRET);
-            res.json({token})
+                
+                //Registro correcto
+                let token = jwt.sign({//Token de sesion (tambien lo retorna login)
+                    email: user.email
+                }, process.env.JWT_SECRET);
+                res.json({ token })
+            });
         }
     }
 })
